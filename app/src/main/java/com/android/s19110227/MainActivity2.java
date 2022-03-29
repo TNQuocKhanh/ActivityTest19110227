4beade34145412c076a2e594b56bcbf01f075ab4
TeaNCuteK <87935949+TNQuocKhanh@users.noreply.github.com>
2022-03-29 22:08:22 +0700

    Update MainActivity2.java
    Final commit

--- app/src/main/java/com/android/s19110227/MainActivity2.java
+++ app/src/main/java/com/android/s19110227/MainActivity2.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class MainActivity2 extends AppCompatActivity {
 
     int REQUEST_CODE_CAMERA = 1111;
